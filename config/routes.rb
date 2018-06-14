21d92d5cee5ace8048c970cc6024d55e389cee55
Dmitry Pchellovod <pchellovod@gmail.com>
2018-06-14 12:49:11 +0000

    change tests and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
-  get  'static_pages/contact'
 
+  get  '/help',    to: 'static_pages#help', as: :help_path
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
